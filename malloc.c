15327558833b82a34a6af9bb7fc742856e5da336
goals <goals@goalsdeMacBook-Pro.local>
2018-05-21 07:56:27 +0800

    raw source

---
+++ malloc.c
@@ -0,0 +1,195 @@
+/*
+实现一个简单的内存管理模块，即malloc 与 free
+真正的malloc 内存不够用的时候会sbrk向系统申请，上限是虚拟内存的最大值。
+而这个实现事先申请好最大的内存，作为虚拟内存的上限，sbrk的时候仅增大指针值即可。
+*/
+#include <stduni.h>
+
+
+int mm_init(void);
+void* mm_malloc(size_t size);
+void mm_free(void* ptr);
+
+static char* mem_heap;//指向堆底
+static char* mem_brk;//指向当前堆顶+1
+static char* mem_max_addr;//最大的虚拟内存地址+1
+
+#define MAX_HEAP 100*1024*1024 //100M
+#define WSIZE 4//BYTES 字
+#define DSIZE 8//BYTES 双字
+#define CHUNKSIZE (1<<12)//4*1024=4KB
+#define MAX(x,y) ((x)>(y)?(x):(y))
+
+/*将头块/脚块的size和状态整合成一个4字节*/
+#define PACK(size,alloc) ((size)|(alloc))
+
+/*获取或写入地址 4个字节*/
+#define GET(p) (*(unsigned *)(p))
+#define PUT(p,val) (*(unsigned *)(p)=(val))
+
+/*从头块或脚块获取块大小和是否分配*/
+#define GET_SIZE(p) (GET(p)&~0x7)
+#define GET_ALLOC(p) (GET(p)&~0x1)
+
+/*通过块指针计算头块和脚块*/
+#define HDRP(bp) ((char*)(bp)-WSIZE)
+#define FTRP(bp) ((char*)(bp)+GET_SIZE(HDRP(bp))-DSIZE)
+
+/*通过块指针计算下一个块指针和上一个块指针*/
+#define NEXT_BLKP(bp) ((char*)(bp)+GET_SIZE(((char*)(bp)-WSIZE)))
+#define PREV_BLKP(bp) ((char*)(bp)-GET_SIZE(((char*)(bp)-DSIZE)))
+
+/*
+模拟虚拟内存的最大值，100M
+*/
+void mem_init(void)
+{
+    mem_heap=(char*)malloc(MAX_HEAP);
+    mem_brk=mem_heap;
+    mem_max_addr=(char*)(mem_heap+MAX_HEAP);
+}
+
+void *mem_sbrk(int incr)
+{
+    char* old_brk=mem_brk;
+    if((incr<0)||((mem_brk+incr)>mem_max_addr))
+    {
+        errno=ENOMEM;
+        fprintf(stderr,"error:mem_sbrk failed.Run out of memory\n");
+        return (void*)-1;
+    }
+    mem_brk+=incr;
+    return (void*)old_brk;
+}
+
+
+/*创建空闲链表*/
+int mm_init(void)
+{
+    if((heap_listtp=mem_sbrk(4*WSIZE))==(void*)-1)
+    {
+        return -1;
+    }
+    PUT(heap_listtp,0);
+    PUT(heap_listtp+(1*WSIZE),PACK(DSIZE,1));
+    PUT(heap_listtp+(2*WSIZE),PACK(DSIZE,1));
+    PUT(heap_listtp+(3*WSIZE),PACK(0,1));
+    heap_listtp+=(2*WSIZE);
+    //扩展空的堆
+    if(extern_heap(CHUNKSIZE/WSIZE)==NULL)
+    {
+        return -1;
+    }
+    return 0;
+}
+
+//双字的倍数对齐--与块的最小值16字节没关系？
+//堆初始化和mm_malloc找不到合适的匹配块时，调用xtend_heap向系统申请内存
+static void * extend_heap(size_t words)
+{
+    char* bp;
+    size_t size;
+
+    size=(words%2)?(words+1)*WSIZE:words*WSIZE;
+    if((long)(bp=mem_sbrk(size))==-1)
+    {
+        return NULL;
+    }
+
+    PUT(HDRP(bp),PACK(size,0));
+    PUT(FTRP(bp),PACK(size,0));
+    PUT(HDRP(NEXT_BLKP(bp)),PACK(0,1));
+
+    return coalesce(bp);
+}
+
+/*释放和合并块
+将头和脚块置为可用，再看能否合并
+*/
+void mm_free(void* bp)
+{
+    size_t size=GET_SIZE(HDRP(bp));
+
+    PUT(HDRP(bp),PACK(size,0));
+    PUT(FTRP(bp),PACK(size,0));
+    coalesce(bp);
+}
+
+static void* coalesce(void *bp)
+{
+    size_t prev_alloc=GET_ALLOC(FTRP(PREV_BLKP(bp)));
+    size_t next_alloc=GET_ALLOC(HDRP(NEXT_BLKP(bp)));
+    size_t size=GET_SIZE(HDRP(bp));
+
+    if(prev_alloc&&next_alloc)//前后都已经分配
+    {
+        return bp;
+    }
+    else if(prev_alloc&&!next_alloc)
+    {
+        siez+=GET_SIZE(HDRP(NEXT_BLKP(bp)));
+        PUT(HDRP(bp),PACK(size,0));
+        PUT(FTRP(bp),PACK(size,0));
+    }
+    else if(!prev_alloc&&next_alloc)
+    {
+        size+=GET_SIZE(HDRP(PREV_BLKP(bp)));
+        PUT(HDRP(bp),PACK(size,0));
+        PUT(FTRP(bp),PACK(size,0));
+    }
+    else
+    {
+        size+=GET_SIZE(HDRP(PREV_BLKP(bp)))+
+            GET_SIZE(FTRP(NEXT_BLKP(bp)));
+        PUT(HDRP(PREV_BLKP(bp)),PACK(size,0));
+        PUT(FTRP(NEXT_BLKP(bp)),PACK(size,0));
+        bp=PREV_BLKP(bp);
+    }
+    return bp;
+}
+
+/*分配块，16字节对齐 即2倍双字*/
+void *mm_malloc(size_t size)
+{
+    size_t asize;//ajust block size
+    size_t extendsize;
+    char* bp;
+
+    if(size==0)
+    {
+        return NULL;
+    }
+    if(size<=DSIZE)
+    {
+        asize=2*DSIZE;
+    }
+    else
+    {
+        asize=DSIZE*((size+(DSIZE)+(DSIZE-1))/DSIZE);
+    }
+
+    if((bp=find_fit(asize))!=NULL)
+    {
+        place(bp,asize);
+        return bp;
+    }
+    extendsize=MAX(asize,CHUNKSIZE);
+    if((bp=extend_heap(extendsize/WSIZE))==NULL)
+    {
+        return NULL;
+    }
+    place(bp,asize);
+    return bp;
+}
+
+/*放置并分割*/
+bool place(char* bp,size_t asize)
+{
+
+}
+
+/*首次适配搜索*/
+char* find_fit(size_t asize)
+{
+
+}
